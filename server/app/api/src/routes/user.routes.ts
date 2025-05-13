18c73c94cb86b9509bc3a8ede10b65cc9406b3ac
kev-69 <bismarkobuobi19@gmail.com>
2025-05-13 08:51:28 +0000

    Refactor user routes to improve readability and maintainability

--- server/app/api/src/routes/user.routes.ts
+++ server/app/api/src/routes/user.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express';
 import { authRoutes } from '../../../../modules/auth/auth.module';
+import { validateToken } from '../../../../middlewares/user.middleware';
 
 const router = Router();
 
-router.use('/auth', authRoutes);
+router.use('/auth',
+    authRoutes
+);
+
+// router.use('/products',
+//     productModule,
+// )
 
 export { router as userRoutes };
