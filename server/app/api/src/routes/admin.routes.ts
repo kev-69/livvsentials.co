0d2b1b964815e8e172cf3672c8545dc96474bfdc
kev-69 <bismarkobuobi19@gmail.com>
2025-05-13 13:39:33 +0000

    Remove token validation middleware from admin routes

--- server/app/api/src/routes/admin.routes.ts
+++ server/app/api/src/routes/admin.routes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 import { adminRoutes } from "../../../../modules/admin/admin.modules";
-import { validateToken } from "../../../../middlewares/user.middleware";
-// import { isAdmin } from "../../../../modules/admin/admin.middleware";
 
 const router = Router();
 
 router.use('/', 
-    validateToken,
     adminRoutes
 );
 
