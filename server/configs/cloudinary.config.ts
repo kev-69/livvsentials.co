9f64bc9ae12a8b6c066e3e245131a25318a5468a
kev-69 <bismarkobuobi19@gmail.com>
2025-05-13 08:51:36 +0000

    Add Cloudinary configuration and image upload utility

---
+++ server/configs/cloudinary.config.ts
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// configuring cloudinary with environment variables
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
+
+// function to upload image to cloudinary
+export const uploadImage = async (filePath: string) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder: 'farmhouse-products',
+    });
+    // remove the file from local storage after uploading to cloudinary
+    fs.unlinkSync(filePath);
+
+    return result.secure_url;
+  } catch (error) {
+    if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath); // remove the file if it exists
+    }
+
+    console.error('Error uploading image to Cloudinary:', error);
+    throw new Error('Error uploading image to Cloudinary');
+  }
+};
