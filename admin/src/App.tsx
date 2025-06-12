b367272e01871559dfa23e97a56204b9ff26f914
kev-69 <bismarkobuobi19@gmail.com>
2025-06-12 10:57:17 +0000

    feat: add Toaster component for notifications in the App

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from './pages/Auth'
 import Dashboard from './pages/Dashboard'
 import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <Toaster richColors position="top-right" />
         <Routes>
           <Route path="/" element={<Auth />} />
           <Route path="/dashboard" element={<Dashboard />} />
