494ece79919340deceee5ea5dfda60caf14c8351
kev-69 <bismarkobuobi19@gmail.com>
2025-06-22 08:44:17 +0000

    feat: refactor admin routes to include validation and authorization middleware

--- server/app/api/src/routes/admin.routes.ts
+++ server/app/api/src/routes/admin.routes.ts
@@ -1,10 +1,48 @@
 import { Router } from "express";
-import { adminRoutes } from "../../../../modules/admin/admin.modules";
+import { authRoutes } from "../../../../modules/admin/auth/auth.module";
+
+import { isAdmin, validateToken } from "../../../../middlewares/admin.middleware";
+
+import { categoryRoutes } from "../../../../modules/admin/categories/category.module";
+import { userRoutes } from "../../../../modules/admin/users/user.module";
+import { productRoutes } from "../../../../modules/admin/products/product.module";
+import { paymentRoutes } from "../../../../modules/admin/payments/payment.module";
+import { orderRoutes } from "../../../../modules/admin/orders/order.module";
 
 const router = Router();
 
+router.use('/',
+    authRoutes
+)
+
 router.use('/', 
-    adminRoutes
+    validateToken,
+    isAdmin,
+    productRoutes
 );
+
+router.use('/',
+    validateToken,
+    isAdmin,
+    categoryRoutes
+)
+
+router.use('/',
+    validateToken,
+    isAdmin,
+    userRoutes
+)
+
+router.use('/',
+    validateToken,
+    isAdmin,
+    paymentRoutes
+)
+
+router.use('/',
+    validateToken,
+    isAdmin,
+    orderRoutes
+)
 
 export { router as adminRoutes };
