fd6e3cd22a302d115c55c173cd1dbb4a5a3d2c6a
kev-69 <bismarkobuobi19@gmail.com>
2025-06-22 08:49:01 +0000

    feat: add order module with routes for managing orders and their statuses

---
+++ server/modules/admin/orders/order.module.ts
@@ -0,0 +1,106 @@
+import { Router } from "express";
+import { z } from 'zod'
+
+import { orderController } from "./order.controller";
+
+// import middlewares
+import { validateRequest } from "../../../middlewares/admin.middleware";
+
+
+const router = Router();
+
+// ======== ORDER-RELATED ROUTES ========
+/**
+ * @route GET /api/admin/orders/
+ * @desc Get all orders
+ * @access Admin only
+*/
+router.get('/orders',
+    orderController.getOrders
+);
+
+/**
+ * @route GET /api/admin/orders/stats
+ * @desc Get order chart data
+ * @access Admin only
+*/
+router.get('/orders/stats',
+    orderController.getOrdersChart
+);
+
+/**
+ * @route GET /api/admin/orders/stats/this-week
+ * @desc Get orders for this week
+ * @access Admin only
+*/
+router.get('/orders/stats/this-week',
+    orderController.getOrdersThisWeek
+);
+
+/**
+ * @route GET /api/admin/orders/stats/avg-weekly
+ * @desc Get average weekly orders
+ * @access Admin only
+*/
+router.get('/orders/stats/avg-weekly',
+    orderController.getAvgWeeklyOrders
+);
+
+/**
+ * @route GET /api/admin/orders/pending
+ * @desc Get pending orders
+ * @access Admin only
+*/
+router.get('/orders/pending',
+    orderController.getPendingOrders
+);
+
+/**
+ * @route GET /api/admin/orders/:orderId
+ * @desc Get a particular order with ID
+ * @access Admin only
+*/
+router.get('/orders/:orderId',
+    validateRequest({
+        params: z.object({ orderId: z.string().uuid() }),
+    }),
+    orderController.getOrderById
+);
+
+/**
+ * @route POST /api/admin/orders/:orderId/ship
+ * @desc Update order status to shipped using ID
+ * @access Admin only
+*/
+router.post('/orders/:orderId/ship',
+    validateRequest({
+        params: z.object({ orderId: z.string().uuid() }),
+    }),
+    orderController.shipOrder
+);
+
+/**
+ * @route POST /api/admin/orders/:orderId/cancel
+ * @desc Update order status to cancelled using ID
+ * @access Admin only
+*/
+router.post('/orders/:orderId/cancel',
+    validateRequest({
+        params: z.object({ orderId: z.string().uuid() }),
+    }),
+    orderController.cancelOrder
+);
+
+/**
+ * @route POST /api/admin/orders/:orderId/deliver
+ * @desc Update order status to delivered using ID
+ * @access Admin only
+*/
+router.post('/orders/:orderId/deliver',
+    validateRequest({
+        params: z.object({ orderId: z.string().uuid() }),
+    }),
+    orderController.deliverOrder
+);
+
+export { router as orderRoutes };
