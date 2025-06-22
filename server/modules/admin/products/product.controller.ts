c7b6f6e639677c30505796d6013962b2497a3760
kev-69 <bismarkobuobi19@gmail.com>
2025-06-22 08:47:29 +0000

    feat: add topSellingProducts method to retrieve and respond with top selling products

--- server/modules/admin/products/product.controller.ts
+++ server/modules/admin/products/product.controller.ts
@@ -88,4 +88,19 @@ export const productController = {
             }
         }
     },
+
+    topSellingProducts: async (req: Request, res: Response) => {
+        try {
+            const products = await productService.getTopSellingProducts();
+            res.status(200).json(successResponse("Top selling products retrieved successfully", products));
+        } catch (error) {
+            if (error instanceof AppError) {
+                res.status(error.statusCode).json(errorResponse(error.message));
+            } else if (error instanceof Error) {
+                res.status(500).json(errorResponse(error.message));
+            } else {
+                res.status(500).json(errorResponse('Internal server error'));
+            }
+        }
+    }
 };
