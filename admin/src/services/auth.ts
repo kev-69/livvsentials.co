4b4d72ccef014a28c3c6599ec209a52fcfed9570
kev-69 <bismarkobuobi19@gmail.com>
2025-06-23 13:12:20 +0000

    refactor: comment out debug logs in loginAdmin and add updateProfile and changePassword functions

--- admin/src/services/auth.ts
+++ admin/src/services/auth.ts
@@ -2,9 +2,9 @@ import { api } from "@/lib/api"
 
 export const loginAdmin = async (email: string, password: string) => {
     try {
-        console.log('Attempting login with:', { email, password: '***' });
+        // console.log('Attempting login with:', { email, password: '***' });
         const response = await api.post('/admin/login', { email, password });
-        console.log('Login response:', response.data);
+        // console.log('Login response:', response.data);
         const data = response.data.data;
         if (data && data.token) {
             localStorage.setItem('adminToken', data.token);
@@ -24,4 +24,34 @@ export const loginAdmin = async (email: string, password: string) => {
 
 export const logoutAdmin = async() => {
     localStorage.removeItem('adminToken')
+}
+
+export const updateProfile = async (profileData: any) => {
+    try {
+        const response = await api.put(`/admin/profile`, profileData);
+        return response.data.data;
+    } catch (error: any) {
+        console.error('Error updating profile:', error);
+        if (error.response) {
+            console.error('Error response data:', error.response.data);
+            console.error('Error response status:', error.response.status);
+            console.error('Error response headers:', error.response.headers);
+        }
+        throw error;
+    }
+}
+
+export const changePassword = async (currentPassword: string, newPassword: string) => {
+    try {
+        const response = await api.post('/admin/reset-password', { currentPassword, newPassword})
+        return response.data.message
+    } catch (error: any) {
+        console.error('Error updating profile:', error);
+        if (error.response) {
+            console.error('Error response data:', error.response.data);
+            console.error('Error response status:', error.response.status);
+            console.error('Error response headers:', error.response.headers);
+        }
+        throw error;
+    }
 }
