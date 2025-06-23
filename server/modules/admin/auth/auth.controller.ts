337cc25d03075e2a014d104359df3878203fb14b
kev-69 <bismarkobuobi19@gmail.com>
2025-06-23 14:40:26 +0000

    feat: refactor auth controller to integrate authService and enhance error handling in profile and password management functions

--- server/modules/admin/auth/auth.controller.ts
+++ server/modules/admin/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Response, Request } from "express";
-import { login } from "./auth.service";
-import { fetchProfile } from "./auth.service";
+import { authService, login } from "./auth.service";
 import { z } from "zod";
 import { AppError } from "../../../utils/errors";
 import { successResponse, errorResponse } from "../../../utils/response";
@@ -35,22 +34,68 @@ export const loginAdmin = async (req: Request, res: Response) => {
     }
 }
 
-export const fetchAdminProfile = async (req: Request, res: Response) => {
-    try {
-        const admin = await fetchProfile(req.user.id);
-        if (!admin) {
-            throw new AppError('Admin not found', 404);
+export const authController = {
+    fetchProfile: async (req: Request, res: Response) => {
+        try {
+            const admin = await authService.fetchProfile(req.user.id)
+            if (!admin) {
+                throw new AppError('Admin not found', 404);
+            }
+            res.status(200).json(
+                successResponse('Admin profile fetched successfully', admin),
+            );
+        } catch (error) {
+            if (error instanceof AppError) {
+                res.status(error.statusCode).json(errorResponse(error.message));
+            } else if (error instanceof Error) {
+                res.status(400).json(errorResponse(error.message));
+            }  else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
         }
-        res.status(200).json(
-            successResponse('Admin profile fetched successfully', admin),
-        );
-    } catch (error) {
-        if (error instanceof AppError) {
-            res.status(error.statusCode).json(errorResponse(error.message));
-        } else if (error instanceof Error) {
-            res.status(400).json(errorResponse(error.message));
-        }  else {
-            res.status(400).json({ message: 'An unknown error occurred' });
+        
+    },
+
+    changePassword: async (req: Request, res: Response) => {
+        try {
+            const { currentPassword, newPassword } = req.body
+            const password = await authService.changePassword(req.user.id, currentPassword, newPassword)
+            res.status(200).json(
+                successResponse('Password changed successfully', password),
+            );
+        } catch (error) {
+            if (error instanceof AppError) {
+                res.status(error.statusCode).json(errorResponse(error.message));
+            } else if (error instanceof Error) {
+                res.status(400).json(errorResponse(error.message));
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
+        }
+    },
+
+   
+    updateProfile: async (req: Request, res: Response) => {
+        try {
+            const profileData = req.body;
+            
+            // Add the uploaded file to the profile data
+            if (req.file) {
+                profileData.files = [req.file];
+            }
+            
+            const updatedProfile = await authService.updateProfile(req.user.id, profileData);
+            res.status(200).json(
+                successResponse('Profile updated successfully', updatedProfile),
+            );
+        } catch (error) {
+            if (error instanceof AppError) {
+                res.status(error.statusCode).json(errorResponse(error.message));
+            } else if (error instanceof Error) {
+                res.status(400).json(errorResponse(error.message));
+            } else {
+                res.status(400).json({ message: 'An unknown error occurred' });
+            }
         }
     }
 }
