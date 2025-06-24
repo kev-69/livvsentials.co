5e0857c8a405e73363937087d0575a8c09c3daa0
kev-69 <bismarkobuobi19@gmail.com>
2025-06-24 09:44:40 +0000

    feat: implement user controller with methods for retrieving users, user statistics, and guest checkouts

---
+++ server/modules/admin/users/user.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+import { userServices } from "./user.service";
+import { AppError } from "../../../utils/errors";
+import { successResponse, errorResponse } from "../../../utils/response";
+
+export const userController = {
+    getAllUsers: async (req: Request, res: Response) => {
+        try {
+            const users = await userServices.getAllUsers();
+            res.status(200).json(successResponse("Users retrieved successfully", users));
+        } catch (error) {
+            if (error instanceof AppError) {
+                res.status(error.statusCode).json(errorResponse(error.message));
+            } else if (error instanceof Error) {
+                res.status(500).json(errorResponse(error.message));
+            } else {
+                res.status(500).json(errorResponse('Internal server error'));
+            }
+        }
+    },
+    
+    getUserStats: async (req: Request, res: Response) => {
+        try {
+            const stats = await userServices.getUserStats();
+            res.status(200).json(successResponse("User statistics retrieved successfully", stats));
+        } catch (error) {
+            if (error instanceof AppError) {
+                res.status(error.statusCode).json(errorResponse(error.message));
+            } else if (error instanceof Error) {
+                res.status(500).json(errorResponse(error.message));
+            } else {
+                res.status(500).json(errorResponse('Internal server error'));
+            }
+        }
+    },
+    
+    getGuestCheckouts: async (req: Request, res: Response) => {
+        try {
+            const guestStats = await userServices.getGuestCheckouts();
+            res.status(200).json(successResponse("Guest checkout statistics retrieved successfully", guestStats));
+        } catch (error) {
+            if (error instanceof AppError) {
+                res.status(error.statusCode).json(errorResponse(error.message));
+            } else if (error instanceof Error) {
+                res.status(500).json(errorResponse(error.message));
+            } else {
+                res.status(500).json(errorResponse('Internal server error'));
+            }
+        }
+    },
+}
