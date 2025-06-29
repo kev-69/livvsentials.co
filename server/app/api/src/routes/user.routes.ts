29818b8b1978fb3f87fbf0258cbbd1481a9aca76
kev-69 <bismarkobuobi19@gmail.com>
2025-06-29 07:50:18 +0000

    feat: add missing user routes for products, orders, addresses, and wishlist

--- server/app/api/src/routes/user.routes.ts
+++ server/app/api/src/routes/user.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { authRoutes } from '../../../../modules/users/auth/auth.module';
-import { validateToken } from '../../../../middlewares/user.middleware';
 import { getSettingsRoutes } from '../../../../modules/users/platform/platform.module';
+import { productRoutes } from '../../../../modules/users/products/product.module';
+import { orderRoutes } from '../../../../modules/users/orders/order.module';
+import { addressRoutes } from '../../../../modules/users/address/address.module';
+import { wishlistRoutes } from '../../../../modules/users/wishlist/wishlist.module';
+import { categoryRoutes } from '../../../../modules/users/categories/category.module';
 
 const router = Router();
 
@@ -14,7 +18,26 @@ router.use('/settings',
     getSettingsRoutes
 )
 
-// all other routes that require validation and authentication
+// categories routes
+router.use('/categories',
+    categoryRoutes
+)
 
+// product routes
+router.use('/products',
+    productRoutes
+);
+
+router.use('/orders',
+    orderRoutes
+);
+
+router.use('/addresses',
+    addressRoutes
+)
+
+router.use('/wishlist',
+    wishlistRoutes
+)
 
 export { router as userRoutes };
